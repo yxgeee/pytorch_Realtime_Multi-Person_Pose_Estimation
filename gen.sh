1de68b325f496718315c9d2a3663cb16f251f2d9
Yixiao Ge <geyixiao831@gmail.com>
2019-09-17 19:06:38 +0800

    Update gen.sh

--- gen.sh
+++ gen.sh
@@ -1,3 +1,3 @@
-srun -p OCR --gres=gpu:1 -n1 --ntasks-per-node=1 \
-  python landmark.py /mnt/lustre/geyixiao/ECCV2018/pytorch-CycleGAN-and-pix2pix/datasets/market/raw/Market-1501-v15.09.15/query \
-                      /mnt/lustre/geyixiao/ECCV2018/pytorch-CycleGAN-and-pix2pix/datasets/market/raw/land/query
+CUDA_VISIBLE_DEVICES=0 \
+  python landmark.py geyixiao/pytorch-CycleGAN-and-pix2pix/datasets/market/raw/Market-1501-v15.09.15/query \
+                      geyixiao/pytorch-CycleGAN-and-pix2pix/datasets/market/raw/land/query
